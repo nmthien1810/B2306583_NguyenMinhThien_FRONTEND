50d77975d0ea48cebf6956c53f4044232c1de0b0
Nguyen Minh Thien <nmthien1810@gmail.com>
2026-07-08 17:51:25 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: {
+    modelValue: { type: String, default: "" },
+  },
+  emits: ["submit", "update:modelValue"],
+  methods: {
+    updateModelValue(e) {
+      this.$emit("update:modelValue", e.target.value);
+    },
+    submit() {
+      this.$emit("submit");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="input-group">
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Nhập thông tin cần tìm"
+      :value="modelValue"
+      @input="updateModelValue"
+      @keyup.enter="submit"
+    />
+    <button class="btn btn-outline-secondary" type="button" @click="submit">
+      <i class="fas fa-search"></i> Tìm kiếm
+    </button>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");
